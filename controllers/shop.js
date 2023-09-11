a5c29d1fbcadae9805cc3233acea338b4266f73e
Vivek gupta <me.vivekgupta9@gmail.com>
2023-09-11 19:03:37 +0530

    dynamic route task 1

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,7 +10,7 @@ exports.getProducts = (req, res, next) => {
 	});
 };
 
-exports.getProduct = (req, res) => {
+exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
 	Product.findById(prodId, (product) => {
 		console.log(product);
